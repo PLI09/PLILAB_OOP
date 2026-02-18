3ab68550f3498da558ebeedca0e7bf9a19d6651b
lip4@tpu.ru <podop@LAPTOP-3T5CBGKC>
2026-02-19 00:16:40 +0700

    feature(Lab2) : лабораторная работа №2

---
+++ Lab2/LB2/Gender.cs
@@ -0,0 +1,18 @@
+﻿namespace LB2 
+{
+    /// <summary>
+    /// Пол человека
+    /// </summary>
+    public enum Gender 
+    {
+        /// <summary>
+        /// Мужской пол
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// Женский пол
+        /// </summary>
+        Female
+    } 
+}
