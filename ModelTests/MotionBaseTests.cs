39b8b5aa830e8d9635e50124b44158b9a3490390
alexey.kalentyev <alexey.kalentyev@50ohm.tech>
2026-06-12 13:42:07 +0700

    ЛР6 принята

--- ModelTests/MotionBaseTests.cs
+++ ModelTests/MotionBaseTests.cs
@@ -93,7 +93,6 @@ namespace ModelTests
             "специфическую информацию")]
         public void GetInfo_ContainsBaseInfo()
         {
-            //TODO: RSDN+
             const double x0 = 10;
             const double t = 3;
             const double v = 7;
